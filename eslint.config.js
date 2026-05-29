12cd167deab45ff3961b4294d6bcad146b891d45
Pavel Ashifin <globus40000@gmail.com>
2026-05-29 19:55:31 +0600

    chore(lint): enable React-specific lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -15,6 +17,8 @@ export default defineConfig([
       tseslint.configs.stylisticTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactX.configs["strict-type-checked"],
+      reactDom.configs.strict,
     ],
     languageOptions: {
       parserOptions: {
